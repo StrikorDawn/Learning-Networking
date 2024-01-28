5c44b4d4cc401e100a8f4fb07b1dc473610a932c
Taden Marston <113535943+StrikorDawn@users.noreply.github.com>
2024-01-27 17:35:25 -0700

    Updated code documentation and made sure it functions as expected

--- FileSharingApp.py
+++ FileSharingApp.py
@@ -1,93 +1,107 @@
 import socket
-
+# Initialization Variables
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 12345
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
+#Size limit for the files being transered
 SIZE = 1024
+# Variables for Configuration
+# Will utilize them more in the future for GUI implimentation.
 sFile = "Send\Test.txt"
 destination = "Recieve\\"
 
+# Start up navigation, user selectes if they are hosting or connecting to the other program.
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
+    option = 0
+    # Allows for multiple files to be sent without rebooting the program multiple times
+    while option != 3:
+        # Creates a break to make the terminal more readable
+        print("")
+        # Display options
+        print("What would you like to do?")
+        print("1. Recieve File")
+        print("2. Send File")
+        print("3. Quit")
+        option = Select()
+        if option == 1:
+            ReciveFile()
+        elif option == 2:
+            SendFile()
+        elif option == 3:
+            # Makes it so the program finishes correctly without prompting the user with an error
+            pass
+        else:
+            print("Please input valid option. (1, 2 or 3)")
+            Select()
 
-    file = open("data/test.txt", "r")
-    data = file.read()
-
-    client.send("test.txt".encode(FORMAT))
-    msg = client.recv(SIZE).decode(FORMAT)
-    print(f"[SERVER]: {msg}")
-import socket
-
-IP = socket.gethostbyname(socket.gethostname())
-PORT = 12345
-ADDR = (IP, PORT)
-FORMAT = "utf-8"
-SIZE = 2048
-
-
-def main():
-    print("The revieving side needs to start their program first.")
-    print("1. Recieve File")
-    print("2. Send File")
-    Select()
-
+# Takes the input from the user and call the appropriate funcion.
 def Select():
-    option = int(input("Are you sending or receiving a file? "))
-    if option == 1:
-        ReciveFile()
-    elif option == 2:
-        SendFile()
-    else:
-        print("Please input valid option. (1 or 2)")
-        Select()
-
+    option = int(input("> "))
+    return option 
+    
 def ReciveFile():
     print("[STARTING] Server is starting...")
+    # Initiates the server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Displays the users IP so they can share it to the sender.
     print(f"Give this to the Sender: {IP}")
+    # Opens port for file traffic
     server.bind(ADDR)
+    # Listens for incoming connection attempts
     server.listen()
-    print("[LISTENING] Server is listening...")
+    print("[LISTENING] Server is listening...")\
+    # Creates Exit var for file sending
+  
+    # Accepts incomming connection requests over the specified port
+    conn, addr = server.accept()
+    # Displays to the user the IP of the connecting computer.
+    print(f"[NEW CONNECTION] {addr} connected.")
+    # saves the file infromation to a variable to be writen to the specified location.
+    filename = conn.recv(SIZE).decode(FORMAT)
+    print(f"[RECV] {filename} Recieved")
+    file = open(f"Recieve\{filename}", 'w')
+    # Returns that the File has been recieved
+    conn.send(f"{filename} received".encode(FORMAT))
+    # Retrieves the information that is to be written to the file
+    data = conn.recv(SIZE).decode(FORMAT)
+    print(f"[RECV] File data received.")
+    # Writes the text to the file that was sent
+    file.write(data)
+    # Retruns to the other user that the file data has been written and recieved.
+    conn.send("File data received.".encode(FORMAT))
+    # Closes the transfed file
+    file.close()
+    # Disconnects the other user from the Host
+    conn.close()
+    print(f"[DISCONNECTED] {addr} disconnected")
 
-    while True:
-        conn, addr = server.accept()
-        print(f"[NEW CONNECTION] {addr} connected.")
-
-        filename = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] {filename} Recieved")
-        file = open(f"Recieve\{filename}", 'w')
-        conn.send(f"{filename} received".encode(FORMAT))
-
-        data = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] File data received.")
-        file.write(data)
-        conn.send("File data received.".encode(FORMAT))
-
-        file.close()
-        conn.close()
-        print(f"[DISCONNECTED] {addr} disconnected")
 
 def SendFile():
+    # User inputs Host IP recived from Host.
     Host = input("What is the Host's IP: ")
+    # Opens Server Socket
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connects to the provided IP if accepted
     client.connect(ADDR)
-
+    # Reads the file that has been selected for sending
     file = open(sFile, "r")
+    # Reads the contents of the file for sending
     data = file.read()
-
+    # Sends File
     client.send("test.txt".encode(FORMAT))
+    # Displays back to the user if the file was recived
     msg = client.recv(SIZE).decode(FORMAT)
     print(f"[SERVER]: {msg}")
-
+    # Sends file data
     client.send(data.encode(FORMAT))
+    # Displays Message from host that file has been recieved.
     msg = client.recv(SIZE).decode(FORMAT)
     print(f"[SERVER]: {msg}")
-
+    # Closes File
     file.close()
+    # Closes Connection and port for file sharing
     client.close()
-    
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,38 +1,30 @@
 # Overview
 
-{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+This software was built to send files peer to peer for easier sharing with less reliance on the cloud.
 
-{Provide a description the networking program that you wrote. Describe how to use your software.  If you did Client/Server, then you will need to describe how to start both.}
-
-{Describe your purpose for writing this software.}
-
-{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running (you will need to show two pieces of software running and communicating with each other) and a walkthrough of the code.}
+I built this program as an easier way for me to share files with my close friends and family, as well as a way for me to gain a better understanding of how networking functions so I can utilize it in the future.
 
 [Software Demo Video](http://youtube.link.goes.here)
 
 # Network Communication
 
-{Describe the architecture that you used (client/server or peer-to-peer)}
-
-{Identify if you are using TCP or UDP and what port numbers are used.}
-
-{Identify the format of messages being sent between the client and server or the messages sent between two peers.}
+This software is utilizing a peer to peer TCP server. I utilized port 12345 to send files to and from the computers.
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
+Visual Studio Code
 
-{Describe the programming language that you used and any libraries.}
+Python 3
+
+Python Sockets Library
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+* [Low-level Networking interface](https://docs.python.org/3/library/socket.html)
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
-* Item 1
-* Item 2
-* Item 3
+* Build a GUI
+* Modify it to support all file types
+* Find a way to make it an exe file.
